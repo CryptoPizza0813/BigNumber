acbf51cb270496efd7af5e317f3b9a112ee895d1
jhdh0813 <56108428+jhdh0813@users.noreply.github.com>
2020-11-22 19:09:25 +0900

    1122
    division first version

--- bignum_div.c
+++ bignum_div.c
@@ -1,5 +1,33 @@
 #include "bignum_div.h"
 
+word L_D_A(word A1, word A0, word B)   // 함수의 자료형으로 word 사용 가능?{
+{
+    int upper = (1 << (WORD_BITLEN - 1));    // 2^{w-1}
+    int a_j;    // A0의 j번째 비트를 저장할 변수            
+    int temp = 1;
+    word Q = 0;
+    word R = A1;
+
+    for(int j = WORD_BITLEN - 1; j >= 0; j--) {
+
+        // a_j 구하기
+        temp = 1  << j;
+        a_j = A0 & temp;   // j 번째 인덱스의 비트와 AND 수행
+
+
+        if (R >= upper) {
+            Q += (1 << j);
+            R = 2 * R + a_j - B;
+        }
+        else {
+            R = 2 * R + a_j;
+        }
+
+    }
+
+    return Q;
+}
+
 void bi_divcc(bigint* A, bigint* B, bigint** Q, bigint** R) // 0 <= R < B, Q \in 0 ~ W 인 Q, R를 어떻게 반환할지 잘 생각해보기
 {
     int n = A->wordlen;
@@ -15,11 +43,7 @@ void bi_divcc(bigint* A, bigint* B, bigint** Q, bigint** R) // 0 <= R < B, Q \in
             w_Q = WORD_MASK;
         }
         else {
-            w_Q = (A->a[m] * sizeof(word) + A->a[m-1]) / B->a[m-1]; 
-            // Pseudo Code의 line 8
-            // 우선, Q는 word이다. 그런데 W와 A[m]을 곱할때 W를 넘어갈수도 있는데, 상관이 없는 부분인가?
-            // ㄴㄴ 강의노트 보니까 LINE 8 부분은  binary long division with 2 word로 구현해야함. 97페이지 참고!
-            // 나중에 구현 ㄱㄱ
+            w_Q = L_D_A(A->a[m], A->a[m], B->a[m-1]);
         }
 
         // R = A - BQ를 위한 구조체 생성
@@ -55,6 +79,89 @@ void bi_divcc(bigint* A, bigint* B, bigint** Q, bigint** R) // 0 <= R < B, Q \in
 
 void bi_divc(bigint* A, bigint* B, bigint** Q, bigint** R)
 {
+    if (Compare_ABS(A, B) != 1) {
+        bi_set_zero(Q);
+        bi_assign(R, A);
+        return;
+    }
 
-    
+    int m = B->wordlen; // bigint B의 워드길이는 m이다
+    int upper = (1 << (WORD_BITLEN - 1));  // 2^{w-1} 생성
+
+    word B_m1 = B->a[m - 1];
+
+    // compute k such that 2^k * B_{m-1} >= 2^{w-1}
+    int k = 0;
+    while(1) {
+        if (B_m1 >= upper)
+            break;
+        else
+        {
+            k += 1;
+            B_m1 = B_m1 << k;
+        }
+        
+    }
+
+    bigint* A_dash = NULL;
+    bigint* B_dash = NULL;
+    bi_assign(&A_dash, A);
+    bi_assign(&B_dash, B);
+    Left_Shift(&A_dash, k);
+    Left_Shift(&B_dash, k);    
+
+    bigint* Q_dash = NULL;
+    bigint* R_dash = NULL;    
+    bi_new(&Q_dash, 1);
+    bi_new(&R_dash, 1);
+    bi_divcc(A_dash, B_dash, &Q_dash, &R_dash);
+
+    Right_Shift(&R_dash, k);
+    bi_assign(Q, Q_dash);
+    bi_assign(R, R_dash);
+
+    bi_delete(&A_dash);
+    bi_delete(&B_dash);
+    bi_delete(&Q_dash);
+    bi_delete(&R_dash);
+
+}
+
+
+void DIV(bigint* A, bigint* B, bigint** Q, bigint** R)
+{
+    if(B->sign == NEGATIVE) {
+        printf("invalid division\n");
+        return;
+    }
+
+    if(Compare_ABS(A, B) != 1) {
+        bi_set_zero(Q);
+        bi_assign(R, A);
+    }
+
+    bi_set_zero(Q);
+    bi_set_zero(R);
+
+    int n = A->wordlen;
+    int W = (1 << WORD_BITLEN); // 이러면 W?
+
+    bigint* A_i = NULL;
+    bigint* Q_i = NULL;
+    bigint* R_temp = NULL;
+    bi_set_zero(&A_i);
+    bi_set_zero(&Q_i);
+    for(int i = n - 1; i >= 0; i--) {
+        Right_Shift(R, W);
+        A_i->a[0] = A->a[i];
+        Q_i->a[0] = (*Q)->a[i];
+        bi_self_add(R, A_i);
+        bi_assign(&R_temp, *R);
+        bi_divc(R_temp, B, &Q_i, R);
+        (*Q)->a[i] = Q_i->a[0];
+    }
+
+    bi_delete(&A_i);
+    bi_delete(&Q_i);
+    bi_delete(&R_temp);
 }
--- bignum_test.h
+++ bignum_test.h
@@ -1,1 +1,9 @@
-#include "bignum_mul.c"
+#include "bignum_div.c"
+
+void basic_test();
+
+void add_test();
+
+void sub_test();
+
+void mul_test();
