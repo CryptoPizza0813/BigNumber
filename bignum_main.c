88ab7d3fe2f079e8fe99ae2c60d9b74f8a230359
jhdh0813 <56108428+jhdh0813@users.noreply.github.com>
2020-11-27 02:02:12 +0900

    1127
    develope :
    binary string input bignumber
    decimal string input bignumber
    hex string input bignumber
    64-bit version bignumber error corrected
    Karatsuba division completed!
    
    // we are fixing school book division

--- bignum_main.c
+++ bignum_main.c
@@ -4,10 +4,11 @@
  * 2020-11-13
  * */
 #include "bignum_test.c"
-
 int main()
 {
+	
 	srand((unsigned)time(NULL));
+	
 	
 	while(1) {			
 		int cases = 0;
@@ -48,34 +49,41 @@ int main()
 		
 	}
 	
+	
 	/*
+	// 에러 검사용 주석
 	bigint* A = NULL;
 	bigint* B = NULL;
 	bigint* C = NULL;
 	bigint* R = NULL;
 	bigint* Q = NULL;
 
-	word arr1[3] = { 0x12345678, 0x000cdef, 0x000cdef };
-	word b[2] = { 0x9f4265a8, 0x9a53b531 };
-	word c[2] = { 0x53256, 0x9a53b531 };
-	bi_new(&A, 3);
-	bi_set_by_array(&A, 0, arr1, 3);
-	bi_new(&B, 2);
-	bi_set_by_array(&B, 0, b, 2);
+	word arr1[2] = { 0x9abcdef09abcdef0, 0x58659653a8659653 };//, 0x1234567812345678};
+	word b[1] = { 0xa534629815346298 }; //, 0xa8659653a8659653};
+	word c[2] = { 0x23a569bd23a569bd, 0x99a5b63299a5b632 };
+	bi_new(&A, 2);
+	bi_set_by_array(&A, 0, arr1, 2);
+	bi_new(&B, 1);
+	bi_set_by_array(&B, 0, b, 1);
 	bi_new(&C, 2);
 	bi_set_by_array(&C, 0, c, 2);
-	bi_divc(A, B, &Q, &R);
 	printf("\na = 0x");
 	bi_show(A, 16);
 	printf("\nb = 0x");
 	bi_show(B, 16);
+
+	L_D_A(A, b[0], &Q);
+	bi_divcc(A, B, &Q, &R);
+	
 	printf("\nq = 0x", Q);
 	bi_show(Q, 16);
 	printf("\nr = 0x", Q);
 	bi_show(R, 16);
-	printf("\nprint(r == a- (b*q))\n");
-	
-	bi_divc(A, C, &Q, &R);
+	printf("\nprint(q == a//b)\n");
+	printf("print(r == a - b*q)\n");
+
+	bi_divcc(A, C, &Q, &R);
+
 	printf("\na = 0x");
 	bi_show(A, 16);
 	printf("\nb = 0x");
@@ -84,8 +92,31 @@ int main()
 	bi_show(Q, 16);
 	printf("\nr = 0x", Q);
 	bi_show(R, 16);
-	printf("\nprint(r == a- (b*q))\n");
+	printf("\nprint(q == a//b)");
+	printf("\nprint(r == a - b*q)\n");
+	
 	*/
+
+	
+	char a[] = "3456789876543456";
+
+	bigint* Teemo = NULL;
+
+	bi_set_by_string(&Teemo, NON_NEGATIVE, a, 10);
+
+
+	
+	bi_show(Teemo, 2);
+	printf("\n");
+
+
+	bi_show(Teemo, 16);
+	printf("\n");
+
+
+
 	printf("Thanks for using!\n");
+	
+
 	return 0;
 }
